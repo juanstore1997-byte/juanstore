6cc85202ea51a7b465a8507d91e5cf05d64e47dc
editorialheredia-sketch <editorialheredia@gmail.com>
2026-06-22 18:06:25 -0400

    CRITICAL FIX: call initAdmin() on server start

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
+const { initAdmin } = require('./db/database');
+initAdmin();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
